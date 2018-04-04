05e6bf425299c531ec111e9c3f8a8c8431e9fc83
Martin Geier <martin.geier@streamunlimited.com>
2018-04-04 15:40:50 +0200

    board: sue: s1832: detect pressed update button on boot
    Signed-off-by: Martin Geier <martin.geier@streamunlimited.com>

--- board/streamunlimited/common/carrier_generic_board.c
+++ board/streamunlimited/common/carrier_generic_board.c
@@ -1,17 +1,51 @@
 #include <common.h>
+#include <asm/gpio.h>
 
 #include "device_interface.h"
 
+#define FWUP_TIMEOUT	3000	/* in ms */
+#define FWUP_GPIO GPIOAO_3
+#define FWUP_GPIO_NAME "GPIOAO_3"
 
 static int fwup_request = 0;
+static int fwup_initialized = 0;
 
 static int generic_board_init(const struct sue_device_info *device)
 {
+	if (gpio_request(FWUP_GPIO, FWUP_GPIO_NAME)) {
+		printf("fwup failed to request gpio: %s\n", FWUP_GPIO_NAME);
+		return 1;
+	}
+
+	if (gpio_direction_input(FWUP_GPIO)) {
+		printf("fwup failed to set direction for gpio: %s\n", FWUP_GPIO_NAME);
+		return 1;
+	}
+
+	fwup_initialized = 1;
 	return 0;
 }
 
 static int generic_board_late_init(const struct sue_device_info *device)
 {
+	int totaldelay = 0;
+	int gpio_was_pressed = 0;
+
+	while (fwup_initialized && gpio_get_value(FWUP_GPIO)) {
+		gpio_was_pressed = 1;
+
+		if (totaldelay > FWUP_TIMEOUT && !fwup_request) {
+			printf("fwup request set, release NPB_IN now\n");
+			fwup_request = 1;
+		}
+
+		mdelay(100);
+		totaldelay += 100;
+	}
+
+	if (gpio_was_pressed)
+		mdelay(100);
+
 	return 0;
 }
 
